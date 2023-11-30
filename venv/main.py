f63f075bc00f6ca2490120cdb47bd67e48c37162
Carlos Henrique <127338261+chsouza1@users.noreply.github.com>
2023-11-30 17:47:30 -0300

    Adicionando os cauculos

--- venv/main.py
+++ venv/main.py
@@ -7,28 +7,30 @@ def calculadora_notas():
             nota_prova = float(input("Digite a nota que obteve na prova atual: "))
             porcentagem_trabalho = int(input("Digite o modelo de porcentagem dos trabalho: "))
             nota_trabalho = float(input("Digite qual sua nota no trabalho: "))
-            if 0 <= nota_prova or porcentagem_prova or nota_aleatoria or porcentagem_trabalho or nota_trabalho <= 10:
+            if 0 <= nota_prova <= 10 and 0 <= porcentagem_prova <= 100 and 0 <= nota_trabalho <= 10 and 0 <= porcentagem_trabalho <= 100:
+                break
                 ## aqui divide a porcentagem que o aluno colocou no começo do programa
                 porcentagem_prova /= 100
                 ## aqui ele multiplica a nota da prova com a porcentagem dividida.
-                nota_prova *= porcentagem_prova
+                calculo_nota = nota_prova * porcentagem_prova
                 nota_necessaria = 6.0
-                resultado_prova = porcentagem_prova
+                resultado_prova = calculo_nota
+                ## calculo do trabalho
+                porcentagem_trabalho /= 100
+                calculo_trabalho = nota_trabalho * porcentagem_trabalho
+                resultado_trabalho = calculo_trabalho
                 print(f"O calculo da sua prova com a porcentagem da materia fica: {resultado_prova:.3f}")
+                print(f"O calculo da seu trabalho com a porcentagem da materia fica: {resultado_trabalho:.3f}")
 
                 ##calculo previsto
                 nota_aleatoria = float(input("Digite qual a nota mais baixa que voce consegue tirar na prova: "))
-                nota_aleatoria *= porcentagem_prova
-
-
-                break
+                calculo_aleatoria = nota_aleatoria * porcentagem_prova
+                calculo_completo = calculo_aleatoria + resultado_trabalho + resultado_prova
+                print(f"Essa é a sua nota calculando tudo: {calculo_completo:.2f}")
             else:
                 print("Favor, digite uma nota válida entre 0 e 10.")
         except ValueError:
             print("Por favor, digite um valor numérico válido.")
-
-    print(f"A nota que voce precisa tirar na prova é: {resultado_prova:.3f}")
-    print(f"A nota ajustada no trabalho é: {nota_trabalho:.3f}")
 
 # Chama a função principal
 calculadora_notas()
